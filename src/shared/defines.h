aaedb3ec40b4dacc3c85d72b0641fff16eff6fe5
CharaVerKys <charaverk@gmail.com>
2026-03-12 17:20:06 +0300

    why i made this func lul

--- src/shared/defines.h
+++ src/shared/defines.h
@@ -1,19 +1,12 @@
 #pragma once
 
-#include "cussert.hpp"
 #include <log/write>
 using write_ = cvk::write;
 
-inline cvk::write write_serv(){
-#ifdef IT_IS_CLIENT
-    cuabort("call write serv on client");
-#endif
-    return cvk::write(clt::server);
-}
-
-inline cvk::write write_clnt(){
 #ifdef IT_IS_SERVER
-    cuabort("call write clnt on server");
+#define write_serv() cvk::write(clt::server)
 #endif
-    return cvk::write(clt::client);
-}
+
+#ifdef IT_IS_CLIENT
+#define write_clnt() cvk::write(clt::client)
+#endif
